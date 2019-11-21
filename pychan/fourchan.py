058438a1d220d391e769e0059e68ecabede2fa59
shiimizu <shiimizu@protonmail.com>
2019-11-20 21:31:30 -0800

    Update tests

--- pychan/fourchan.py
+++ pychan/fourchan.py
@@ -3,23 +3,22 @@
 from __future__ import absolute_import
 from __future__ import print_function
 import os
-import sys
 import json
-import time
 import ast
-import bigjson
+
 
 class Json():
     db: dict
+
     def load(self, db):
-        if isinstance(db,str):
+        if isinstance(db, str):
             if os.path.exists(db):
                 with open(db, 'rb') as f:
                     try:
                         return json.loads(f.read())
                     except Exception as e:
                         return ast.literal_eval(f.read().decode())
-            else: # If path doesn't exist. Could be a JSON string.
+            else:  # If path doesn't exist. Could be a JSON string.
                 try:
                     return json.loads(db)
                 except Exception as e:
@@ -30,76 +29,120 @@ class Json():
     def __str__(self):
         """Flatmap this object to primitive values and then to a string"""
         return json.dumps(self.__dict__, default=lambda o: o.__dict__, indent=2)
-    
+
     @property
     def json(self):
         """Return data structure as json dictionary.
         Useful for manually accessing keys/values"""
         return json.loads(self.__str__())
-    
+
+
 class BigJson(Json):
     def load(self, db):
-        if isinstance(db,str):
+        if isinstance(db, str):
             if os.path.exists(db):
                 with open(db, 'rb') as f:
                     try:
                         return json.loads(f.read())
                     except Exception as e:
                         return ast.literal_eval(f.read().decode())
-            else: # If path doesn't exist
+            else:  # If path doesn't exist
                 try:
                     return json.loads(db)
                 except Exception as e:
                     raise Exception(e)
         else:
             return db
-    
+
+
 class Post(Json):
     def __init__(self, db):
         self.__dict__.update(self.load(db))
+
 
 class Thread(Json):
     def __init__(self, db):
         self.posts = [Post(x) for x in self.load(db)['posts']]
 
+
 class ThreadIndex(Json):
     def __init__(self, db):
         self.threads = [Thread(x) for x in self.load(db)['threads']]
 
+
 class Board(Json):
+    """The directory the board is located in."""
+    board: str
+
     def __init__(self, db):
         self.__dict__.update(self.load(db))
+
+        """Docstring for class variable A.x"""
+        self.y = 1
+
 
 class TrollFlags(Json):
     def __init__(self, db):
         self.__dict__.update(self.load(db))
-        
+
+
+class Boards(list):
+    def __init__(self):
+        super().__init__()
+        with open("tests/boards.json", mode='r', encoding='utf-8') as f:
+            j = json.load(f)
+            self.__db__: dict = j
+            for a in j.get('boards', None):
+                self.append(a)
+
+    def huh(self):
+        """List of all meme flags"""
+        return
+
+    def __str__(self):
+        return json.dumps(self.__db__, indent=2)
+
+    @property
+    def trollflags(self):
+        """List of all meme flags"""
+        return {}
+
+    @property
+    def memeflags(self):
+        """List of all meme flags"""
+        return self.trollflags
+
+
 class BoardList(Json):
     def __init__(self, db):
-        self.boards = [Board(x) for x in self.load(db)['boards']] 
+        self.boards = [Board(x) for x in self.load(db)['boards']]
         self.trollflags = TrollFlags(self.load(db)['troll_flags'])
 
 
 class Page(Json):
     def __init__(self, db):
         self.page = self.load(db)['page']
-        self.threads = [Post(x) for x in self.load(db)['threads']] 
-        
+        self.threads = [Post(x) for x in self.load(db)['threads']]
+
+
 class Catalog(Json):
     def __init__(self, db):
-        #print(type(self.load(db)[0]['threads'][0]))
-        self.pages = len(self.load(db))-1
+        # print(type(self.load(db)[0]['threads'][0]))
+        self.pages = len(self.load(db)) - 1
         self.page = [Page(p) for p in self.load(db)]
-        
+
+
 class ThreadList(Json):
     # Same as Catalog
     def __init__(self, db):
-        self.pages = len(self.load(db))-1
+        self.pages = len(self.load(db)) - 1
         self.page = [Page(p) for p in self.load(db)]
+
 
 class ArchivedThread(Json):
     def __new__(self, db):
-        return self.load(self,db)
+        return self.load(self, db)
+
 
 class FourChan(Json):
     def __new__(self, db):
@@ -107,28 +150,34 @@ class FourChan(Json):
         self.db = db
         if isinstance(db, dict):
             # Single Thread
-            if db.get('posts', None): return Thread(db)
+            if db.get('posts', None):
+                return Thread(db)
 
             # Thread Index
-            elif db.get('threads', None): return ThreadIndex(db)
+            elif db.get('threads', None):
+                return ThreadIndex(db)
 
             # Board List
-            elif db.get('boards', None): return BoardList(db)
-            
-            else: raise Exception("Unkown input type")
-            
+            elif db.get('boards', None):
+                return BoardList(db)
+
+            else:
+                raise Exception("Unkown input type")
+
         elif isinstance(db, list):
             if len(db) <= 0: raise Exception("List is empty!")
             # Board Catalog
-            if db[0].get('page', None): return Catalog(db)
-            
+            if db[0].get('page', None):
+                return Catalog(db)
+
             # Thread List
-            elif ['no','last_modified','replies'] == [element for element in db[0]['threads'][0]]: return ThreadList(db)
-            
+            elif ['no', 'last_modified', 'replies'] == [element for element in db[0]['threads'][0]]:
+                return ThreadList(db)
+
             # Archived Threads
             else:
-                newdb = [s for s in db if not isinstance(s,int)]
+                newdb = [s for s in db if not isinstance(s, int)]
                 if len(db) != 0 and len(newdb) == 0: return ArchivedThread(db)
-            
+
         else:
             raise Exception("Unknown input type: {}".format(type(db)))
--- tests.py
+++ tests.py
@@ -4,57 +4,95 @@ import time
 import os
 import sys
 import shutil
+import json
 import pychan
 import pytest
+
 
 def test_fourchan_empty_dict():
     with pytest.raises(Exception):
         thread = pychan.FourChan("{}")
-        
+
+
 def test_fourchan_unknown_dict():
     with pytest.raises(Exception):
         thread = pychan.FourChan("""{ "zxc": "qwe", "cvz": 123 }""")
+
+
 def test_fourchan_unknown_string():
     with pytest.raises(Exception):
         thread = pychan.FourChan("""n78yo342v>{?>L}?~!@#+_)|}{\\|""")
-        
+
+
 def test_fourchan_empty_list():
     with pytest.raises(Exception):
         thread = pychan.FourChan("[]")
-        
+
+
 def test_fourchan_unknown_list():
     with pytest.raises(Exception):
         thread = pychan.FourChan("[1]")
 
+
 def test_fourchan_unknown_list_2():
     with pytest.raises(Exception):
         thread = pychan.FourChan("[1,2,34,5]")
-        
+
+
 def test_fourchan_unknown_list_3():
     with pytest.raises(Exception):
         thread = pychan.FourChan("['zxc', 'qwe']")
-    
-#@pytest.mark.xfail(raises=Exception)
+
+
+# @pytest.mark.xfail(raises=Exception)
 def test_fourchan_parse_fuuka_thread():
     with pytest.raises(Exception):
         thread = pychan.FourChan("tests/desu_thread.json")
-    
+
+
 def test_fuuka_thread_conversion():
     fuukaThread = pychan.Fuuka("tests/desu_thread.json")
     assert isinstance(fuukaThread, pychan.fourchan.Thread)
-    
+
+
 def test_fuuka_thread_to_json_conversion():
     fuukaThread = pychan.Fuuka("tests/desu_thread.json")
     assert isinstance(fuukaThread.json, dict)
-    
-def main():
-    fuukaThread = pychan.Fuuka("desu_thread.json")
-    fthread1 = pychan.Fuuka({"desu_thread.json" : "out1.json"})
-    fthread = pychan.FourChan("out1.json")
-    #print(repr(str(fthread1.posts[1])))
-    
+
+
+def test_fourchan_boards():
+    boards = pychan.fourchan.Boards()
+    # boards = pychan.FourChan("tests/boards.json")
+    assert isinstance(boards, list)
+    # assert isinstance(boards.boards[0].cooldowns, str)
+
+
+def test_fourchan_boards_trollflags():
+    boards = pychan.fourchan.Boards()
+    assert isinstance(boards.trollflags, dict)
+
+
+def test_fourchan_boards_memeflags():
+    boards = pychan.fourchan.Boards()
+    assert isinstance(boards.memeflags, dict)
+
+
+def test_fourchan_boards_first():
+    boards = pychan.fourchan.Boards()
+    assert boards[0]['board'] == '3'
+
+
+def test_main():
+    fuukaThread = pychan.Fuuka("tests/desu_thread.json")
+    # fthread1 = pychan.Fuuka({"desu_thread.json" : "out1.json"})
+    # fthread = pychan.FourChan("out1.json")
+    # print(repr(str(fthread1.posts[1])))
+
     # repr(str(fthread1.posts[1]))
-    assert str(fthread1.posts[1]) == """'{\n  "doc_id": 259727564,\n  "name": "Anonymous",\n  "comment": ">>192166777\\nShould have watched Madoka instead.",\n  "comment_sanitized": ">>192166777\\nShould have watched Madoka instead.",\n  "com": "<span class=\\"greentext\\"><a href=\\"http://desuarchive.org/a/thread/192166777/#192166777\\" class=\\"backlink op\\" data-function=\\"highlight\\" data-backlink=\\"true\\" data-board=\\"a\\" data-post=\\"192166777\\">&gt;&gt;192166777</a></span><br />\\nShould have watched Madoka instead.",\n  "no": 192166871,\n  "now": "8/19/19(Mon)16:54",\n  "time": 1566248070,\n  "tim": 1566248070000,\n  "resto": 192166777\n}'""".strip("'")
+    assert str(fuukaThread.posts[
+                   1]) == """'{\n  "doc_id": 259727564,\n  "name": "Anonymous",\n  "comment": ">>192166777\\nShould have watched Madoka instead.",\n  "comment_sanitized": ">>192166777\\nShould have watched Madoka instead.",\n  "com": "<span class=\\"greentext\\"><a href=\\"http://desuarchive.org/a/thread/192166777/#192166777\\" class=\\"backlink op\\" data-function=\\"highlight\\" data-backlink=\\"true\\" data-board=\\"a\\" data-post=\\"192166777\\">&gt;&gt;192166777</a></span><br />\\nShould have watched Madoka instead.",\n  "no": 192166871,\n  "now": "8/19/19(Mon)16:54",\n  "time": 1566248070,\n  "tim": 1566248070000,\n  "resto": 192166777\n}'""".strip(
+        "'")
+
 
 if __name__ == '__main__':
     test_main()
